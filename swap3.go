385a8773c3e6edfea6e55bb9aa2cfad2fb61a2c1
Adwait Naik <adwaitnaik2@gmail.com>
2020-10-18 12:45:55 +0530

    Update swap3.go

--- swap3.go
+++ swap3.go
@@ -2,7 +2,6 @@ package main
 import "fmt"
 
 func swap(num1 float64, num2 float64){
-
      num1 = num1*num2
      num2 = num1/num2
      num1 = num1/num2
@@ -11,7 +10,6 @@ func swap(num1 float64, num2 float64){
      fmt.Print("After swapping the second number is", num2)
 }
 func main(){
-
      num1 := 66.3     
      num2 := 44.3
      swap(num1, num2)
