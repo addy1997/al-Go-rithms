f1313a087891bb8e2ee22f3cbe04961d0b3e11a4
Adwait Naik <adwaitnaik2@gmail.com>
2020-10-18 12:35:22 +0530

    Update num_avg.go

--- num_avg.go
+++ num_avg.go
@@ -1,19 +1,16 @@
 package main
-
 import "fmt"
 
 func average(xs []float64) float64 {
     
     total := 0.0
     for _, v:= range xs {
-        total +=v
+        total+=v
     }
     return total/ float64(len(xs))
 }
 
-
 func main() {
-    
-    xs: = []float64{98,93,77,82,83}
+    xs := []float64{98,93,77,82,83}
     fmt.Println(average(xs))
 }
