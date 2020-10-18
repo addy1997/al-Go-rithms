7afe8636575d7238e0d6b3d439c2fc1aaec555f1
Adwait Naik <adwaitnaik2@gmail.com>
2020-10-18 12:41:26 +0530

    Update palindrome.go

--- palindrome.go
+++ palindrome.go
@@ -1,18 +1,12 @@
 package main
 
-import (
-
-
-      "fmt"
-)
+import "fmt"
 
 func Palindrome(number int) {
 
     var temp,remainder int
     var reverse int = 0
-
     temp = number
-
     for{
         remainder = number%10
         reverse = reverse*10 + remainder
@@ -27,9 +21,7 @@ func Palindrome(number int) {
     }else{
         fmt.Printf("%d is not a Palindrome",temp)
     }
-  
 }    
-
 func main() {
     number:=1661
     Palindrome(number)
