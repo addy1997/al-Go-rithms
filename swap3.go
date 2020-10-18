7ee729faf031a2b7e58e0a54f1fe4b7600e4d224
Adwait Naik <adwaitnaik2@gmail.com>
2020-10-18 09:21:07 +0530

    swap with multi and division

---
+++ swap3.go
@@ -0,0 +1,26 @@
+package main 
+
+import "fmt"
+
+func swap(num1 float64, num2 float64){
+
+     num1 = num1*num2
+
+     num2 = num1/num2
+
+     num1 = num1/num2
+
+     fmt.Print("After swapping the first number is", num1)
+
+     fmt.Print("After swapping the second number is", num2)
+}
+
+func main(){
+
+     num1 := 66.3
+     
+     num2 := 44.3
+
+     swap(num1, num2)
+
+}
