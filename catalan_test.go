87d170f6692870ad7315da761038f2c2720794e2
Adwait Naik <adwaitnaik2@gmail.com>
2020-10-20 08:32:23 +0530

    test for catalan

---
+++ catalan_test.go
@@ -0,0 +1,19 @@
+// reference for testing https://www.calhoun.io/how-to-test-with-go/
+
+package testing101
+
+import (
+
+  "testing"
+)
+
+func TestCatalan(t *testing.T){
+
+  x:=5
+  expected:=42
+  actual:= Catalan(x)
+
+  if actual != expected {
+    t.Errorf("Expected the sum of %d to be %d but instead got %d", x, expected, actual)
+  }
+}
