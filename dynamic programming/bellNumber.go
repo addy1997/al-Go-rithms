5fddea3a2bc594de75e8d082876ee97f5a264d28
Adwait <adwaitnaik2@gmail.com>
2020-11-22 05:01:29 +0530

    bellNumber function

---
+++ dynamic programming/bellNumber.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+
+func bellNumber(n int)int{
+    
+    
+    s := 100
+    var bell =  make([s+1][s+1] int)
+    
+    bell[0][0] = 1
+    
+    var i, j int
+    
+    for i=1; i<=n; i++{
+        
+        bell[i][0] = bell[i-1][i-1]
+        
+    }
+        for j=1; j<=i; j++{
+            
+            bell[i][j] = bell[i-1][j-1] + bell[i][j-1]
+        }
+    
+    return bell[n][0]
+}
+
+func main(){
+    
+    for n:=0;n<=5;n++{
+        
+        fmt.Println("Bell number is",bellNumber(n))
+    }
+}
