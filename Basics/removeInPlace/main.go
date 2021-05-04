1fd127b8996a66e2bd09209924767d6df6689f11
rootVIII <colleyloyejames@gmail.com>
2021-05-04 12:53:18 -0400

    add comments
    Signed-off-by: rootVIII <colleyloyejames@gmail.com>

--- Basics/removeInPlace/main.go
+++ Basics/removeInPlace/main.go
@@ -2,8 +2,20 @@ package main
 
 import "fmt"
 
-// rootVIII remove in place
-// 0 ms, faster than 100.00% of Go online submissions
+/*
+rootVIII remove in place
+0 ms, faster than 100.00% of Go online submissions
+
+LEETCODE: https://leetcode.com/problems/remove-element/
+
+Given an array nums and a value val, remove all
+instances of that value in-place and return the
+new length. Do not allocate extra space for another
+array, you must do this by modifying the input
+array in-place with O(1) extra memory. The order of
+elements can be changed. It doesn't matter what you
+leave beyond the new length.
+*/
 
 func removeElement(nums []int, val int) int {
 	for i := 0; i < len(nums); i++ {
